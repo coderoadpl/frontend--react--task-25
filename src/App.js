b3f2358629d1dc901d1ff1367ebdf360f807fe2c
Mateusz Choma <kontakt@coderoad.pl>
2021-06-14 20:26:11 +0200

    Passing components as props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react'
 
 export const TestProps = (props) => {
+  const { reactComponent: ReactComponent } = props
   return (
     <div>
       <h1>
@@ -12,11 +13,21 @@ export const TestProps = (props) => {
       <p>Boolean: {props.boolean ? 'TRUE' : 'FALSE'}</p>
       <p>Null: {props.null === null ? 'NULL' : 'NOT NULL'}</p>
       <p>Undefined: {props.undefined === undefined ? 'UNDEFINED' : 'NOT UNDEFINED'}</p>
+
       <p>Object: {JSON.stringify(props.object)}</p>
       <p>Object property: {props.object.name}</p>
       <p>Array: {props.array}</p>
       <p>Function: <button onClick={props.function}>CLICK ME</button></p>
+
+      <p>React element: {props.reactElement}</p>
+      <p>React component: <ReactComponent name={'Marek'} /></p>
     </div>
+  )
+}
+
+export const Greeter = (props) => {
+  return (
+    <h2>Hello {props.name}!</h2>
   )
 }
 
@@ -33,6 +44,9 @@ export const App = () => {
         object={{ name: 'Mateusz' }}
         array={['Ala', 'Ola', 'Ela']}
         function={() => console.log('I\'m form props!')}
+
+        reactElement={<Greeter name={'Mateusz'} />}
+        reactComponent={Greeter}
       />
     </div>
   )
